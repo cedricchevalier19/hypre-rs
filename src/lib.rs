3e82cdf649fd90f4f8768d45f6615c7385b796da
Cedric Chevalier <cedric.chevalier@cea.fr>
2022-05-23 23:01:32 +0200

    static dispatch with enum_dispatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,47 @@
+use enum_dispatch::enum_dispatch;
+
+#[enum_dispatch]
+trait LinearSolver {
+    fn solve(self);
+    fn set_precond(&mut self);
+}
+
+struct CGSolver;
+
+impl LinearSolver for CGSolver {
+    fn solve(self) {
+        todo!()
+    }
+
+    fn set_precond(&mut self) {
+        todo!()
+    }
+}
+
+struct AMGSolver;
+
+impl LinearSolver for AMGSolver {
+    fn solve(self) {
+        todo!()
+    }
+
+    fn set_precond(&mut self) {
+        todo!()
+    }
+}
+
+#[enum_dispatch(LinearSolver)]
+enum Solver {
+    CG(CGSolver),
+    AMG(AMGSolver),
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let solver: Solver = CGSolver {}.into();
+        solver.solve();
     }
 }
